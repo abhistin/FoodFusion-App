562d8864fa111f05462268a47149a9eb04a8e9ef
Abhishek Bhardwaj <abhishek.sharmar112@gmail.com>
2023-11-03 08:48:05 +0530

    Add necessary files and create initial launch screen

--- FoodFusion/Views/Onboarding/OnboardingViewController.swift
+++ FoodFusion/Views/Onboarding/OnboardingViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  OnboardingViewController.swift
 //  FoodFusion
 //
 //  Created by Abhishek Bhardwaj on 02/11/23.
@@ -7,13 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class OnboardingViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        
     }
 
-
 }
-
